47c3ed665a20e83035f3d2e283b4af71e83674ea
Lucas Vieira <lucas.vieira94@outlook.com>
2023-05-22 09:46:34 -0300

    Changed `DeltaTableUtils` to add `to_athena_v3` method

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
+## [1.3.0] - 2023-05-22
+
+### Changed
+
+- Changed `DeltaTableUtils` to add `to_athena_v3` method.
+
 ## [1.2.1] - 2023-03-22
 
 ### Fixed
--- databricks_aws_utils/delta_table.py
+++ databricks_aws_utils/delta_table.py
@@ -9,23 +9,23 @@ from databricks_aws_utils import DatabrickAWSUtils
 
 class DeltaTableUtils(DatabrickAWSUtils):
     """
-        Delta Table AWS Integration Utils.
+    Delta Table AWS Integration Utils.
 
-        > This Delta Table integration only works if the Databricks use the AWS Glue as the Metastore
+    > This Delta Table integration only works if the Databricks use the AWS Glue as the Metastore
 
-        Args:
-            spark (`SparkSession`): spark session
-            name: (`str`): delta table name, must contain the database (e.g. `<database>.<table>`)
-            aws_region (`str`, optional): AWS region, default `us-east-1`
-            iam_role (`str`, optional): IAM Role ARN, if specified assumes the IAM role to perform the AWS API calls
-            aws_access_key_id (`str`, optional): Temporary AWS Access Key Id
-            aws_secret_access_key (`str`, optional): Temporary AWS Secret Access Key
-            aws_session_token (`str`, optional): Temporary AWS Session Token
+    Args:
+        spark (`SparkSession`): spark session
+        name: (`str`): delta table name, must contain the database (e.g. `<database>.<table>`)
+        aws_region (`str`, optional): AWS region, default `us-east-1`
+        iam_role (`str`, optional): IAM Role ARN, if specified assumes the IAM role to perform the AWS API calls
+        aws_access_key_id (`str`, optional): Temporary AWS Access Key Id
+        aws_secret_access_key (`str`, optional): Temporary AWS Secret Access Key
+        aws_session_token (`str`, optional): Temporary AWS Session Token
 
-        Features:
+    Features:
 
-        - Convert Databricks delta table to AWS Glue Format using symlink_format_manifest to allow the AWS Athena
-            or Presto to consume externally
+    - Convert Databricks delta table to AWS Glue Format using symlink_format_manifest to allow the AWS Athena
+        or Presto to consume externally
     """
 
     def __init__(
@@ -49,19 +49,62 @@ class DeltaTableUtils(DatabrickAWSUtils):
         self.spark = spark
         self.name = name
 
+    def to_athena_v3(self) -> None:
+        """
+        Dec 19, 2022, AWS announced the Athena V3 engine now supports Delta Lake tables.
+
+        https://aws.amazon.com/about-aws/whats-new/2022/12/athena-enhances-read-support-delta-lake-table-format/
+
+        However, there are some gotchas and requirements in the integrations.
+
+        - The table needs to have a table property called `table_type` with the value `DELTA`.
+        - AWS Glue should synchronize the columns and partitions with the Glue Catalog, however, for some reason \
+            the columns and partitions are not synchronized for all tables, so it is necessary to synchronize \
+            the columns and partitions manually.
+
+        This method adds the `table_type` property to the table and synchronize the columns and partitions.
+        """
+        db_name, table_name = self.name.split('.')
+        columns, partitions = self.schema_to_glue()
+        client = self.session.client('glue')
+        response = client.get_table(
+            DatabaseName=db_name,
+            Name=table_name
+        )
+
+        table = response['Table']
+        del table['CreateTime']
+        del table['UpdateTime']
+        del table['DatabaseName']
+        del table['IsRegisteredWithLakeFormation']
+        del table['CreatedBy']
+        del table['VersionId']
+        del table['CatalogId']
+
+        table['SerdeInfo']['Parameters']['table_type'] = 'DELTA'
+        table['StorageDescriptor']['Columns'] = columns
+        table['PartitionKeys'] = partitions
+
+        self.logger.info("Updating table")
+        client.update_table(
+            DatabaseName=db_name,
+            TableInput=table
+        )
+        self.logger.info("Table successfully updated")
+
     def to_athena(self, target_database: str, target_table: str) -> None:
         """
-            Converts a Delta table to external table using AWS Athena or Presto
-                format using `symlink_format_manifest`
+        Converts a Delta table to external table using AWS Athena or Presto
+            format using `symlink_format_manifest`
 
-            Presto Integration full documentation:
-            <https://docs.databricks.com/delta/presto-integration.html#limitations>
+        Presto Integration full documentation:
+        <https://docs.databricks.com/delta/presto-integration.html#limitations>
 
-            Args:
-                delta_table (`str`): delta table name
-                target_database (`str`): external database name
-                target_table (`str`): external table name
-                target_table_description (`str`, optional): external table description
+        Args:
+            delta_table (`str`): delta table name
+            target_database (`str`): external database name
+            target_table (`str`): external table name
+            target_table_description (`str`, optional): external table description
         """
 
         columns, partitions = self.schema_to_glue()
@@ -94,19 +137,19 @@ class DeltaTableUtils(DatabrickAWSUtils):
 
     def get_table_name(self) -> str:
         """
-            Get delta table name without the database name
+        Get delta table name without the database name
 
-            Returns:
-                `str`: table name
+        Returns:
+            `str`: table name
         """
         return self.name.split('.')[1]
 
     def get_database_name(self) -> str:
         """
-            Get database name from the delta table
+        Get database name from the delta table
 
-            Returns:
-                `str`: database name
+        Returns:
+            `str`: database name
         """
         if '.' not in self.name:
             raise RuntimeError(f"Cannot extract database name from the delta table name '{self.name}'")
@@ -115,10 +158,10 @@ class DeltaTableUtils(DatabrickAWSUtils):
 
     def get_location(self) -> str:
         """
-            Get delta table location
+        Get delta table location
 
-            Returns:
-                `str`: delta table location
+        Returns:
+            `str`: delta table location
         """
         client = self.session.client('glue')
 
@@ -131,10 +174,10 @@ class DeltaTableUtils(DatabrickAWSUtils):
 
     def schema_to_glue(self) -> Tuple[List[dict], List[dict]]:
         """
-            Extracts the delta table schema and returns in the AWS Glue Format
+        Extracts the delta table schema and returns in the AWS Glue Format
 
-            Returns:
-                `Tuple[List[dict], List[dict]]` columns and partitions
+        Returns:
+            `Tuple[List[dict], List[dict]]` columns and partitions
         """
         columns = []
         partitions = []
@@ -165,15 +208,15 @@ class DeltaTableUtils(DatabrickAWSUtils):
         location: str,
     ) -> None:
         """
-            Creates the AWS Glue Table
+        Creates the AWS Glue Table
 
-            Args:
-                database_name (`str`): database name
-                table_name (`str`): table name
-                columns (`List[dict]`): schema columns
-                partitions (`List[dict]`): partitions definition
-                location (`str`): delta table location
-                table_description (`str`, optional): table description
+        Args:
+            database_name (`str`): database name
+            table_name (`str`): table name
+            columns (`List[dict]`): schema columns
+            partitions (`List[dict]`): partitions definition
+            location (`str`): delta table location
+            table_description (`str`, optional): table description
         """
         client = self.session.client('glue')
         self.logger.info(f"Creating table '{database_name}.{table_name}'")
@@ -188,19 +231,18 @@ class DeltaTableUtils(DatabrickAWSUtils):
         table: dict,
         columns: List[dict],
         partitions: List[dict],
-        location: str
+        location: str,
     ):
         """
-            Updates the AWS Glue Table
+        Updates the AWS Glue Table
 
-            Args:
-                session (`Session`): boto3 session
-                database_name (`str`): database name
-                table (`dict`): AWS Glue GetTable operation response
-                columns (`List[dict]`): schema columns
-                partitions (`List[dict]`): partitions definition
-                location (`str`): delta table location
-                table_description (`str`, optional): table description
+        Args:
+            session (`Session`): boto3 session
+            database_name (`str`): database name
+            table (`dict`): AWS Glue GetTable operation response
+            columns (`List[dict]`): schema columns
+            partitions (`List[dict]`): partitions definition
+            location (`str`): delta table location
         """
         client = self.session.client('glue')
         database_name = table['DatabaseName']
@@ -221,16 +263,16 @@ class DeltaTableUtils(DatabrickAWSUtils):
         location: str
     ) -> Dict[str, Any]:
         """
-            Builds the AWS Glue TableInput object.
+        Builds the AWS Glue TableInput object.
 
-            Args:
-                table_name (`str`): table name
-                columns (`List[dict]`): schema columns
-                partitions (`List[dict]`): partitions definition
-                location (`str`): delta table location
+        Args:
+            table_name (`str`): table name
+            columns (`List[dict]`): schema columns
+            partitions (`List[dict]`): partitions definition
+            location (`str`): delta table location
 
-            Returns:
-                `Dict[str, Any]`: AWS Glue TableInput object
+        Returns:
+            `Dict[str, Any]`: AWS Glue TableInput object
         """
         return {
             'Name': table_name,
